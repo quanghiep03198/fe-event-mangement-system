068e9d8bbf679327bbc5c8d348205769c8c64ba3
Quang Hiep <quanghiep03198@gmail.com>
2024-02-20 23:49:27 +0700

    feat: linted stage init

---
+++ src/components/ui/@tawk/index.tsx
@@ -0,0 +1,17 @@
+import { useAppSelector } from '@/redux/hook'
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react'
+import { useEffect, useRef } from 'react'
+
+const TawkMessenger: React.FunctionComponent = () => {
+   const authenticated = useAppSelector((state) => state.auth.authenticated)
+   const ref = useRef<typeof TawkMessengerReact.prototype>()
+
+   useEffect(() => {
+      if (authenticated) ref.current.showWidget()
+      else ref.current.onLoad = ref.current.hideWidget()
+   }, [authenticated])
+
+   return <TawkMessengerReact ref={ref} propertyId={import.meta.env.VITE_TAWK_PROPERTY_ID} widgetId={import.meta.env.VITE_TAWK_WIDGET_ID} />
+}
+
+export default TawkMessenger
