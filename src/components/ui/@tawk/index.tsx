2232f4dab4a9e0cf9c0b19fa13dfcae51b20a814
Quang Hiep <quanghiep03198@gmail.com>
2024-02-21 10:00:28 +0700

    fix: load tawk messenger widget

--- src/components/ui/@tawk/index.tsx
+++ src/components/ui/@tawk/index.tsx
@@ -1,21 +1,7 @@
-import { useAppSelector } from '@/redux/hook'
 import TawkMessengerReact from '@tawk.to/tawk-messenger-react'
-import { useRef } from 'react'
 
 const TawkMessenger: React.FunctionComponent = () => {
-   const authenticated = useAppSelector((state) => state.auth.authenticated)
-   const ref = useRef<typeof TawkMessengerReact.prototype>()
-
-   const handleLoad = () => {
-      if (!ref.current) return
-      console.log(ref.current)
-      if (authenticated) ref.current.showWidget()
-      else ref.current.hideWidget()
-   }
-
-   handleLoad()
-
-   return <TawkMessengerReact ref={ref} propertyId={import.meta.env.VITE_TAWK_PROPERTY_ID} widgetId={import.meta.env.VITE_TAWK_WIDGET_ID} onLoad={handleLoad} />
+   return <TawkMessengerReact propertyId={import.meta.env.VITE_TAWK_PROPERTY_ID} widgetId={import.meta.env.VITE_TAWK_WIDGET_ID} />
 }
 
 export default TawkMessenger
